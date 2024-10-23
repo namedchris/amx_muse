456c0877786dcf36119734160eeeaf48f11483d3
Morgan <crmorga@ilstu.edu>
2024-10-22 19:45:31 -0500

    formatting

--- driver.py
+++ driver.py
@@ -1,12 +1,12 @@
 import muse
 
 
-class DisplayDriver():
+class DisplayDriver:
 
     def __init__(self):
         self.is_powered = False
         self.pic_is_muted = False
-        
+
     def update_state(self, feedback):
         pass
 
@@ -25,30 +25,34 @@ class DisplayDriver():
 
 class LGDriver(DisplayDriver):
 
-    #commands
-    POWER_OFF_COMMAND = 'ka 00 00\x0D'
-    POWER_ON_COMMAND = 'ka 00 01\x0D'
-    PIC_MUTE_OFF_COMMAND = 'kd 0 00\x0D'
-    PIC_MUTE_ON_COMMAND = 'kd 1 01'
-    #acknowledgements
-    POWER_ON_ACK = 'a 01 OK01x\x0D'
-    POWER_OFF_ACK = 'a 01 OK00x\x0D'
-    PIC_MUTE_ON_ACK = 'd 01 OK01x\x0D'
-    PIC_MUTE_OFF_ACK = 'd 01 OK00x\x0D'
+    # commands
+    POWER_OFF_COMMAND = "ka 00 00\x0D"
+    POWER_ON_COMMAND = "ka 00 01\x0D"
+    PIC_MUTE_OFF_COMMAND = "kd 0 00\x0D"
+    PIC_MUTE_ON_COMMAND = "kd 1 01"
+    # acknowledgements
+    POWER_ON_ACK = "a 01 OK01x\x0D"
+    POWER_OFF_ACK = "a 01 OK00x\x0D"
+    PIC_MUTE_ON_ACK = "d 01 OK01x\x0D"
+    PIC_MUTE_OFF_ACK = "d 01 OK00x\x0D"
 
-    def __init__(self,device):
+    def __init__(self, device):
         super.__init__()
-        self.device =  device
+        self.device = device
 
     def update_state(self, cls, feedback):
         lines = feedback.split("\x0D")
         for line in lines:
             match line:
-                case cls.POWER_OFF_ACK: self.is_powered = False
-                case cls.POWER_ON_ACK: self.is_powered = True
-                case cls.PIC_MUTE_OFF_ACK: self.pic_is_muted = False
-                case cls.PIC_MUTE_ON_ACK: self.pic_is_muted = True 
-        
+                case cls.POWER_OFF_ACK:
+                    self.is_powered = False
+                case cls.POWER_ON_ACK:
+                    self.is_powered = True
+                case cls.PIC_MUTE_OFF_ACK:
+                    self.pic_is_muted = False
+                case cls.PIC_MUTE_ON_ACK:
+                    self.pic_is_muted = True
+
     def toggle_power(self):
         if self.is_powered:
             self.device.send(self.POWER_OFF_COMMAND)
@@ -57,10 +61,10 @@ class LGDriver(DisplayDriver):
 
     def power_off(self):
         self.device.send(self.POWER_OFF_COMMAND)
-  
+
     def power_on(self):
         self.device.send(self.POWER_ON_COMMAND)
-   
+
     def toggle_pic_mute(self):
         if self.pic_is_muted:
             return self.PIC_MUTE_OFF_COMMAND
--- index.py
+++ index.py
@@ -1,8 +1,7 @@
-#Todo: put drivers in a driver module where they extend 'Driver'
+# Todo: put drivers in a driver module where they extend 'Driver'
 
 from mojo import context
 import driver
-
 
 
 rooms = []
@@ -16,58 +15,70 @@ power_on_functions = {}
 power_off_functions = {}
 pic_mute_toggle_functions = {}
 
-#if different kinds of displays have different functions, the logic for that goes in this function
-#but it seems simplest to abstract drivers behind a Python class
-def get_display_functions(display): 
+
+# if different kinds of displays have different functions, the logic for that goes in this function
+# but it seems simplest to abstract drivers behind a Python class
+def get_display_functions(display):
     power_toggle = display[1].toggle_power
     power_on = display[1].power_on
     power_off = display[1].power_off
     toggle_pic_mute = display[1].toggle_pic_mute
-    return (power_toggle,power_on,power_off,toggle_pic_mute)
+    return (power_toggle, power_on, power_off, toggle_pic_mute)
 
 
 def display_listener(event, room):
     ui = uis[room]
     try:
-        data = str(event.arguments['data'].decode())
+        data = str(event.arguments["data"].decode())
     except UnicodeDecodeError as err:
         context.log.error(f"{err=}")
         raise
-    items = data.split("\x0D")     
-    if 'touchpad' in ui[0]:
-        #get this rooms display buttons
+    items = data.split("\x0D")
+    if "touchpad" in ui[0]:
+        # get this rooms display buttons
         power_button = uis[room].port[1].channel[9]
         pic_mute_button = uis[room].port[1].channel[210]
-        #update driver state
+        # update driver state
         for item in items:
             displays[room].update_state(item)
-        #update button state    
+        # update button state
         power_button.value = displays[room].power_is_on
         pic_mute_button.value = displays[room].pic_mute_is_on
-    elif 'keypad' in ui[0]:
-        #TODO implement keypad support
+    elif "keypad" in ui[0]:
+        # TODO implement keypad support
         pass
+
 
 def load_devices():
     device_ids = context.devices.ids()
     print(device_ids)
     for device_id in device_ids:
         muse_device = context.devices.get(device_id)
-        #build a set of rooms
-        split_id = device_id.split('-')
-        room_name = ('-'.join(split_id[:2]))
+        # build a set of rooms
+        split_id = device_id.split("-")
+        room_name = "-".join(split_id[:2])
         rooms.append(room_name)
-        #select proper driver based on device_id
-        if 'keypad' in device_id:
-            uis[room_name] = (device_id, muse_device) #extracting the device name from the device object later is trificult
-        elif 'touchpad' in device_id:
-            uis[room_name] = (device_id, muse_device) #so I stash it in a tuple here
-        elif 'monitor' in device_id:
-            displays[room_name] = (device_id, driver.lg_driver(muse_device)) #monitors use a python driver, so I add it to the tuple
-        elif 'projector' in device_id:
-            displays[room_name] = (device_id, driver.epson_driver(muse_device)) #projectors use a duet driver, so they don't need a python driver
-        elif 'switcher' in device_id:
+        # select proper driver based on device_id
+        if "keypad" in device_id:
+            uis[room_name] = (
+                device_id,
+                muse_device,
+            )  # extracting the device name from the device object later is trificult
+        elif "touchpad" in device_id:
+            uis[room_name] = (device_id, muse_device)  # so I stash it in a tuple here
+        elif "monitor" in device_id:
+            displays[room_name] = (
+                device_id,
+                driver.lg_driver(muse_device),
+            )  # monitors use a python driver, so I add it to the tuple
+        elif "projector" in device_id:
+            displays[room_name] = (
+                device_id,
+                driver.epson_driver(muse_device),
+            )  # projectors use a duet driver, so they don't need a python driver
+        elif "switcher" in device_id:
             switchers[room_name] = (device_id, driver.extron_driver(muse_device))
+
 
 def setup_rooms():
     for room in rooms:
@@ -76,15 +87,19 @@ def setup_rooms():
         power_on_functions[room],
         power_off_functions[room],
         pic_mute_toggle_functions[room] = get_display_functions(displays[room])
-        #setup button watchers for room 
-        if 'touchpad' in uis[room[0]]:
+        # setup button watchers for room
+        if "touchpad" in uis[room[0]]:
             buttons = {
-                #muse listeners must accept an event argument. event.value tells you if the you are handling a press or release
-                #executes function on push, executes noop on release
-                "port/1/button/9": lambda event: power_toggle_functions[room] if event.value else None, #a funtion with no return is None, so I think thie works 
-                "port/1/button/210": lambda event: pic_mute_toggle_functions[room] if event.value else lambda: None, #if muse needs a callable lambda: None shoule work as a noop
+                # muse listeners must accept an event argument. event.value tells you if the you are handling a press or release
+                # executes function on push, executes noop on release
+                "port/1/button/9": lambda event: (
+                    power_toggle_functions[room] if event.value else None
+                ),  # a funtion with no return is None, so I think thie works
+                "port/1/button/210": lambda event: (
+                    pic_mute_toggle_functions[room] if event.value else lambda: None
+                ),  # if muse needs a callable lambda: None shoule work as a noop
             }
-        '''
+        """
         elif 'keypad' in uis[room[0]]:
             buttons = {
                 "port/1/button/9": lambda event: power_on_functions[room],
@@ -96,19 +111,18 @@ def setup_rooms():
                 "port/1/button/2": lambda event: pass,
                 "port/1/button/5": lambda event: pass,
             }
-        '''
-        if 'projector' in displays[room[0]]:
+        """
+        if "projector" in displays[room[0]]:
             pass
-        elif 'monitor' in displays[room[0]]:
+        elif "monitor" in displays[room[0]]:
             pass
-        if 'switcher' in switchers[room[0]]:
-            pass    
-        #register watchers
+        if "switcher" in switchers[room[0]]:
+            pass
+        # register watchers
         for key, action in buttons.items():
-            port = int(key.split('/')[1])
-            id = int(key.split('/')[3])
+            port = int(key.split("/")[1])
+            id = int(key.split("/")[3])
             uis[room].port[port].button[id].watch(action)
-
 
     # register feedback listeners with muse devicesa
     displays[room][1].recieve.listen(display_listener)
