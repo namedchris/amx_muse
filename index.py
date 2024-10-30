45389587489f06737ead8adfa89ebf0bc81c8142
Morgan <crmorga@ilstu.edu>
2024-10-30 14:41:53 -0500

    refactored index.py and added tests

--- index.py
+++ index.py
@@ -1,13 +1,7 @@
 from mojo import context
 import drivers
 
-
-rooms = []
-
 uis = {}
-displays = {}
-switchers = {}
-
 
 # create a listener for display feedback
 def get_display_listener(ui, display):
@@ -43,62 +37,80 @@ def get_switcher_listener(ui, switcher):
         print(data)
         switcher.update_state(data)
         if "touchpad" in ui[0]:
-            ui.port[1].channel[31] = switcher.input_three_is_active
-            ui.port[1].channel[32] = switcher.input_four_is_active
-            ui.port[1].channel[33] = switcher.input_six_is_active
-            ui.port[1].channel[26] = switcher.vol_mute_is_active
-            ui.port[1].level[1] = switcher.volume_level
+            ui[1].port[1].channel[31] = switcher.input_three_is_active
+            ui[1].port[1].channel[32] = switcher.input_four_is_active
+            ui[1].port[1].channel[33] = switcher.input_six_is_active
+            ui[1].port[1].channel[26] = switcher.vol_mute_is_active
+            ui[1].port[1].level[1] = switcher.volume_level
         elif "keypad" in ui[0]:
             # TODO implement keypad support
             pass
 
     return listener
 
+#parse a device ID to get room name 
+def parse_device_id(device_id):
+    split_id = device_id.split("-")
+    room_name = "-".join(split_id[:2])
+    return room_name
 
-# populate the lists and dictionaries; create and register watchers and listeners
-def setup_rooms(event=None):
-    print("setting up rooms")
-    device_ids = list(context.devices.ids())
-    # remove built in devices
-    # I need a better method of doing this
-    # Sometimes these change, causing exceptions
-    try:
-        print(device_ids)
-        device_ids.remove("franky")
-        device_ids.remove("idevice")
-        device_ids.remove("led")
-        print(device_ids)
-    except (ValueError, KeyError) as e:
-        print(e)
-    print(device_ids)
+
+# remove built-in muse devices and return a set of rooms
+def prune_devices(devices, prunings):
+    devices = set(devices)
+    prunings = set(prunings)
+    return devices - prunings
+
+
+# parse device IDs to make a set of rooms
+def populate_rooms(devices):
+    rooms = []
+    for device_id in devices:
+        rooms.append(parse_device_id(device_id))
+    return set(rooms)
+
+
+def populate_switchers(device_ids):
+    switchers = {}
     for device_id in device_ids:
         muse_device = context.devices.get(device_id)
-        # build a set of rooms
-        split_id = device_id.split("-")
-        room_name = "-".join(split_id[:2])
-        if not room_name in rooms:
-            print(f"adding {room_name} to room list")
-            rooms.append(room_name)
-            print(f"{rooms=}")
-        # select driver based on device_id
-        if "keypad" in device_id:
-            uis[room_name] = (
-                device_id,
-                muse_device,
-            )
-        elif "touchpad" in device_id:
-            uis[room_name] = (
-                device_id,
-                muse_device,
-            )
-        # can't find a way to get device_id from muse_device object so it gets stashed in a tuple
-        elif "monitor" in device_id:
+        room_name = parse_device_id(device_id)
+        if "switcher" in device_id:
+            switchers[room_name] = (device_id, drivers.ExtronDriver(muse_device))
+    return switchers
+
+def populate_displays(device_ids):
+    displays = {}
+    for device_id in device_ids:
+        muse_device = context.devices.get(device_id)
+        room_name = parse_device_id(device_id)
+        if "monitor" in device_id:
             displays[room_name] = (device_id, drivers.LGDriver(muse_device))
         elif "projector" in device_id:
             # TODO add projector support
             pass
-        elif "switcher" in device_id:
-            switchers[room_name] = (device_id, drivers.ExtronDriver(muse_device))
+    return displays
+
+def populate_uis(device_ids):
+    uis = {}
+    for device_id in device_ids:
+        muse_device = context.devices.get(device_id)
+        room_name = parse_device_id(device_id)
+        if "keypad" in device_id:
+            uis[room_name] = (device_id,muse_device)
+        elif "touchpad" in device_id:uis[room_name] = (device_id,muse_device,)
+        #TODO implement touchpad support
+    return uis
+
+
+# populate the lists and dictionaries; create and register watchers and listeners
+def setup_rooms(event=None):
+    # remove built in devices
+    device_ids = prune_devices(list(context.devices.ids()),('franky','led','idevice'))
+    rooms = populate_rooms(device_ids)
+    switchers = populate_switchers(device_ids)
+    displays = populate_displays(device_ids)
+    uis = populate_uis(device_ids)
     for room in rooms:
         display = displays[room][1]
         switcher = switchers[room][1]
@@ -107,34 +119,16 @@ def setup_rooms(event=None):
             buttons = {
                 # muse listeners must accept an event argument. event.value tells you if the you are handling a press or release
                 # executes function on push, executes noop on release
-                "port/1/button/9": lambda event: (
-                    display.toggle_power() if event.value else None
-                ),
-                "port/1/button/210": lambda event: (
-                    display.toggle_pic_mute() if event.value else None
-                ),
-                "port/1/button/24": lambda event: (
-                    switcher.start_volume_ramp_up()
-                    if event.value
-                    else switcher.stop_volume_ramp_up
-                ),
-                "port/1/button/25": lambda event: (
-                    switcher.start_volume_ramp_down()
-                    if event.value
-                    else switcher.stop_volume_ramp_down
-                ),
-                "port/1/button/26": lambda event: (
-                    switcher.toggle_vol_mute() if event.value else None
-                ),
-                "port/1/button/31": lambda event: (
-                    switcher.select_source_three() if event.value else None
-                ),
-                "port/1/button/32": lambda event: (
-                    switcher.select_source_four() if event.value else None
-                ),
-                "port/1/button/33": lambda event: (
-                    switcher.select_source_six() if event.value else None
-                ),
+                "port/1/button/9": lambda event: (display.toggle_power() if event.value else None),
+                "port/1/button/210": lambda event: (display.toggle_pic_mute() if event.value else None),
+                "port/1/button/24": lambda event: (switcher.start_volume_ramp_up() if event.value 
+                                                   else switcher.stop_volume_ramp_up),
+                "port/1/button/25": lambda event: (switcher.start_volume_ramp_down() if event.value 
+                                                   else switcher.stop_volume_ramp_down),
+                "port/1/button/26": lambda event: (switcher.toggle_vol_mute() if event.value else None),
+                "port/1/button/31": lambda event: (switcher.select_source_three() if event.value else None),
+                "port/1/button/32": lambda event: (switcher.select_source_four() if event.value else None),
+                "port/1/button/33": lambda event: (switcher.select_source_six() if event.value else None),
             }
         # register watchers
         for key, action in buttons.items():
@@ -150,10 +144,8 @@ def setup_rooms(event=None):
             get_switcher_listener(uis[room], switchers[room])
         )
 
-
 # get controller context
 muse = context.devices.get("idevice")
 print("starting script")
 # setup rooms when controller comes online
-setup_rooms()
-# muse.online(setup_rooms)
+muse.online(setup_rooms)
